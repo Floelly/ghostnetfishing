8d25bcfd585a7656071f7fb6f393d98be215191c
Floelly <email@florianbowitz.de>
2025-08-19 11:04:39 +0200

    test and production profile for 2 different databases (currently docker container)

--- src/test/java/dev/floelly/ghostnetfishing/integration/GhostNetStory1Test.java
+++ src/test/java/dev/floelly/ghostnetfishing/integration/GhostNetStory1Test.java
@@ -5,18 +5,24 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@SpringBootTest()
 @AutoConfigureMockMvc
 public class GhostNetStory1Test {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Test
+    void shouldLoadSpringContext() {
+    }
 
     @Test
     void shouldDisplayGhostNetForm() throws Exception {
