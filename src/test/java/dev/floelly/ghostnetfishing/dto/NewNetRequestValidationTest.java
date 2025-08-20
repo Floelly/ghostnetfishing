d4878f374846eb098d94f97017a81d5919a7dac8
Floelly <email@florianbowitz.de>
2025-08-20 21:34:31 +0200

    added new TODO

---
+++ src/test/java/dev/floelly/ghostnetfishing/dto/NewNetRequestValidationTest.java
@@ -0,0 +1,9 @@
+package dev.floelly.ghostnetfishing.dto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NewNetRequestValidationTest {
+
+    //TODO: implement Validation Tests & Validation itself
+
+}
