e5b208ba6f26f41807b7dc5e99b5913688f9c58c
Floelly <email@florianbowitz.de>
2025-08-20 17:49:22 +0200

    RED: refactored ControllerTest + added new

--- src/test/java/dev/floelly/ghostnetfishing/controller/NetsControllerTest.java
+++ src/test/java/dev/floelly/ghostnetfishing/controller/NetsControllerTest.java
@@ -1,21 +1,43 @@
 package dev.floelly.ghostnetfishing.controller;
 
-import org.junit.jupiter.api.BeforeAll;
+import dev.floelly.ghostnetfishing.dto.NewNetRequest;
+import dev.floelly.ghostnetfishing.service.INewNetService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class NetsControllerTest {
-    private static NetsController netController;
+    private final static String NEW_NET_CONTENT_TEMPLATE = "nets/new";
+    private static final String NETS_CONTENT_TEMPLATE = "nets";
+    private static final String POST_NEW_NET_REDIRECT_TEMPLATE = NETS_CONTENT_TEMPLATE;
 
-    @BeforeAll
-    static void beforeAll() {
-        netController = new NetsController();
-    }
+    @Mock
+    INewNetService newNetService;
+
+    @InjectMocks
+    private NetsController netController;
 
     @Test
     void shouldConnectToNewNetForm() {
-        String controllerReturn = netController.getNewNetForm();
-        assertEquals("nets/new", controllerReturn);
+        String controllerResponse = netController.getNewNetFormPage();
+        assertEquals(NEW_NET_CONTENT_TEMPLATE, controllerResponse, String.format("The response of the controller should be '%s'", NEW_NET_CONTENT_TEMPLATE));
+    }
+
+    @Test
+    void shouldCallServiceAndReturnRedirectOnSuccess() {
+        NewNetRequest newNetRequest = new NewNetRequest(20,20,"L");
+        doNothing().when(newNetService).addNewNet(eq(newNetRequest));
+
+        String controllerResponse = netController.postNewNet(newNetRequest);
+
+        verify(newNetService).addNewNet(eq(newNetRequest));
+        assertEquals("redirect:" + POST_NEW_NET_REDIRECT_TEMPLATE, controllerResponse, String.format("The response of the controller should be a 'redirect:%s'", POST_NEW_NET_REDIRECT_TEMPLATE));
     }
 }
