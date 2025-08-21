f88623698373ff248fdf2912c527c414216ac662
Floelly <email@florianbowitz.de>
2025-08-21 09:42:14 +0200

    added tests for validation of NewNetRequest

--- src/test/java/dev/floelly/ghostnetfishing/dto/NewNetRequestValidationTest.java
+++ src/test/java/dev/floelly/ghostnetfishing/dto/NewNetRequestValidationTest.java
@@ -1,16 +1,102 @@
 package dev.floelly.ghostnetfishing.dto;
 
-import org.junit.jupiter.api.Disabled;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 class NewNetRequestValidationTest {
-    //TODO: implement Validation Tests & Validation itself
+    private static final Stream<Double> INVALID_LATITUDES = Stream.of(-90.0001, 90.0001, null);
+    private static final Stream<Double> VALID_LATITUDES = Stream.of(-90.0, 5.000000000001, 90.0);
+    private static final Stream<Double> INVALID_LONGITUDES = Stream.of(-180.1, 180.0000001, null);
+    private static final Stream<Double> VALID_LONGITUDES = Stream.of(-180.0, -32.123456, 180.0);
+    public static final String FIELD_NAME_LONGITUDE = "locationLong";
+    public static final String FIELD_NAME_LATITUDE = "locationLat";
 
-    @Disabled("not implemented jet")
-    @Test
-    void validation() {
+    private static Validator validator;
 
+    private NewNetRequest defaultValidRequest;
+
+    private static Stream<Double> provideInvalidLatitudes() {
+        return INVALID_LATITUDES;
+    }
+
+    public static Stream<Double> provideValidLatitudes() {
+        return VALID_LATITUDES;
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            validator = factory.getValidator();
+        }
+    }
+
+    public static Stream<Double> provideInvalidLongitudes() {
+        return INVALID_LONGITUDES;
+    }
+
+    public static Stream<Double> provideValidLongitudes() {
+        return VALID_LONGITUDES;
+    }
+
+    @BeforeEach
+    void setUp() {
+        defaultValidRequest = new NewNetRequest(0.0, 0.0, "L");
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidLatitudes")
+    void shouldFailForLatitudeOutOfRange(Double lat) {
+        defaultValidRequest.setLocationLat(lat);
+
+        Set<ConstraintViolation<NewNetRequest>> violations = validator.validate(defaultValidRequest);
+
+        assertThat(violations).isNotEmpty();
+        assertThat(violations.stream().map(v -> v.getPropertyPath().toString()))
+                .contains(FIELD_NAME_LATITUDE);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideValidLatitudes")
+    void shouldBeValidLatitude(Double lat) {
+        defaultValidRequest.setLocationLat(lat);
+
+        Set<ConstraintViolation<NewNetRequest>> violations = validator.validate(defaultValidRequest);
+
+        assertThat(violations).isEmpty();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidLongitudes")
+    void shouldFailForLongitudeOutOfRange(Double lon) {
+        defaultValidRequest.setLocationLong(lon);
+
+        Set<ConstraintViolation<NewNetRequest>> violations = validator.validate(defaultValidRequest);
+
+        assertThat(violations).isNotEmpty();
+        assertThat(violations.stream().map(v -> v.getPropertyPath().toString()))
+                .contains(FIELD_NAME_LONGITUDE);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideValidLongitudes")
+    void shouldBeValidLongitude(Double lon) {
+        defaultValidRequest.setLocationLong(lon);
+
+        Set<ConstraintViolation<NewNetRequest>> violations = validator.validate(defaultValidRequest);
+
+        assertThat(violations).isEmpty();
     }
 }
