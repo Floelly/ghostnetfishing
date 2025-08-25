2d3effa1d30d7e41be38e2d08a8982259d533c2e
Floelly <email@florianbowitz.de>
2025-08-25 09:08:17 +0200

    new tests added

--- src/test/java/dev/floelly/ghostnetfishing/integration/GhostNetStory1Test.java
+++ src/test/java/dev/floelly/ghostnetfishing/integration/GhostNetStory1Test.java
@@ -18,7 +18,9 @@ import java.text.DecimalFormatSymbols;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -37,6 +39,9 @@ public class GhostNetStory1Test {
     public static final String SIZE = "size";
     public static final List<String> NEW_NET_PARAMETERS = List.of(LOCATION_LAT, LOCATION_LONG, SIZE);
     public static final String EMPTY_STRING = "";
+    private static final String WRONG_NET_LOCATION_LAT = "91.0";
+    private static final String WRONG_NET_LOCATION_LONG = "-180.1";
+    private static final String WRONG_NET_SIZE = "XXS";
     public static final String NETS_NEW_ENDPOINT = "/nets/new";
     public static final String NETS_ENDPOINT = "/nets";
     public static final String NEW_GHOST_NET_HEADLINE = "Report new ghost net";
@@ -44,6 +49,7 @@ public class GhostNetStory1Test {
     public static final String LONGITUDE = "Longitude";
     public static final String LAYOUT_HTML_TAG = "footer";
     public static final String NEW_NET_FORM_QUERY_SELECTOR = "form[method=post][action='" + NETS_NEW_ENDPOINT + "']";
+    public static final String INVALID_FEEDBACK_QUERY_SELECTOR = ".invalid-feedback";
     private static final Map<String, String> EXPECTED_SIZE_OPTIONS = Map.of(
             "", "- Choose size -",
             "S", "S - (Diameter up to 10 m)",
@@ -199,9 +205,68 @@ public class GhostNetStory1Test {
                 .andExpect(view().name(NETS_NEW_ENDPOINT));
     }
 
+    @Test
+    void shouldDisplayErrorInformation_whenGivenInvalidValues_OnPostNewNet() throws Exception {
+        String content = mockMvc.perform(post(NETS_NEW_ENDPOINT)
+                        .param(LOCATION_LAT, WRONG_NET_LOCATION_LAT)
+                        .param(LOCATION_LONG, WRONG_NET_LOCATION_LONG)
+                        .param(SIZE, WRONG_NET_SIZE))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        Elements form = Jsoup.parse(content).select(NEW_NET_FORM_QUERY_SELECTOR);
+        Elements errorDivs = form.select(INVALID_FEEDBACK_QUERY_SELECTOR);
+
+        assertThat(errorDivs).size().isEqualTo(3);
+
+        List<String> errorMessages = errorDivs.stream().map(Element::text).collect(Collectors.toList());
+
+        assertThat(errorMessages).doesNotContain(EMPTY_STRING);
+    }
+
     @Disabled("not implemented jet")
     @Test
-    void shouldDisplayErrorMessagesInToast_whenGivenInvalidValues_OnPostNewNet() {
+    void shouldNotDisplayAnyToastMessage_onNetsPage() throws Exception {
+        MvcResult result = mockMvc.perform(get(NETS_ENDPOINT))
+                .andExpect(status().isOk())
+                .andReturn();
+        Document doc = Jsoup.parse(result.getResponse().getContentAsString());
+        Element toastContainer = doc.selectFirst(".toast-container");
 
+        assertThat(toastContainer).isNotNull();
+
+        Elements toastMessages = toastContainer.select(".toast");
+
+        assertThat(toastMessages).isEmpty();
+    }
+
+    @Disabled("not implemented jet")
+    @Test
+    void shouldDisplaySuccessInfoToastMessage_onPostNewNet() throws Exception {
+        double lat = getRandomLatitude();
+        double lon = getRandomLongitude();
+        String randomLatitude = formatDouble(lat);
+        String randomLongitude = formatDouble(lon);
+
+        mockMvc.perform(post(NETS_NEW_ENDPOINT)
+                        .param(LOCATION_LAT, randomLatitude)
+                        .param(LOCATION_LONG, randomLongitude)
+                        .param(SIZE, "L"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(NETS_ENDPOINT));
+
+        MvcResult result = mockMvc.perform(get(NETS_ENDPOINT))
+                .andExpect(status().isOk())
+                .andReturn();
+        Document doc = Jsoup.parse(result.getResponse().getContentAsString());
+        Element toastContainer = doc.selectFirst(".toast-container");
+        assertThat(toastContainer).isNotNull();
+        Elements toastMessages = toastContainer.select(".toast");
+
+        assertThat(toastMessages).size().isEqualTo(1);
+
+        String toastHtml = Objects.requireNonNull(toastMessages.first()).toString();
+
+        assertThat(toastHtml).contains("Successfully reported net.");
     }
 }
