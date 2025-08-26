229c5f23bfb24277450822e8c9627274df1678b2
Floelly <email@florianbowitz.de>
2025-08-26 23:39:50 +0200

    implemented form-select for filtering for state & sec:thymeleaf dialect

--- src/main/java/dev/floelly/ghostnetfishing/security/SecurityConfig.java
+++ src/main/java/dev/floelly/ghostnetfishing/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import dev.floelly.ghostnetfishing.model.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
