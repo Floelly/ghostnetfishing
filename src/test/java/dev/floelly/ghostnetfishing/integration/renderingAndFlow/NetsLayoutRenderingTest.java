4e4a92ba40b27b3fbccdd72969e293fd26d106ce
Floelly <email@florianbowitz.de>
2025-08-27 10:40:41 +0200

    RED(story): refactoring of old test and bugfix of interfering tests

--- src/test/java/dev/floelly/ghostnetfishing/integration/renderingAndFlow/NetsLayoutRenderingTest.java
+++ src/test/java/dev/floelly/ghostnetfishing/integration/renderingAndFlow/NetsLayoutRenderingTest.java
@@ -4,54 +4,42 @@ import dev.floelly.ghostnetfishing.testutil.AbstractH2Test;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
-import java.util.Objects;
 
 import static dev.floelly.ghostnetfishing.testutil.TestDataFactory.*;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+@Sql(scripts = "/sql/populate-nets-table-diverse.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
 public class NetsLayoutRenderingTest extends AbstractH2Test {
+    public static final String REPORTED_NET_ID = "1001";
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    void shouldRenderRequestRecoveryButtonWithCorrectId_onNetsPage() throws Exception {
-        //neues Netz eintragen
-        double lat = getRandomLatitude();
-        double lon = getRandomLongitude();
-        String randomLatitude = formatDouble(lat);
-        String randomLongitude = formatDouble(lon);
-        mockMvc.perform(post(NETS_NEW_ENDPOINT)
-                        .param(LOCATION_LAT, randomLatitude)
-                        .param(LOCATION_LONG, randomLongitude)
-                        .param(SIZE, "L")
-                        .with(csrf()));
-
+    @WithMockUser(roles = {STANDARD_ROLE})
+    void shouldRenderRequestRecoveryButtonWithCorrectId_whenLoggedIn_onNetsPage() throws Exception {
         MvcResult result = mockMvc.perform(get(NETS_ENDPOINT))
                 .andReturn();
         Document document = Jsoup.parse(result.getResponse().getContentAsString());
-        Elements tableRows = document.select("main tbody tr");
-        assertThat(tableRows).isNotEmpty();
-        assertThat(tableRows.size()).as("Should only display one net entry").isEqualTo(1);
-        Element row = tableRows.first();
-        assertNotNull(row);
+        Element matchingRow = document.select("main tbody tr[data-net-id=" + REPORTED_NET_ID + "]").stream()
+                .findFirst()
+                .orElseThrow(() -> new AssertionError("No Net found for lat/lon"));
 
-        String id = row.attr("data-net-id");
+        String id = matchingRow.attr("data-net-id");
         assertThat(id).isNotEmpty();
 
-        Element form = row.selectFirst("form[method=post][action$=/request-recovery]");
+        Element form = matchingRow.selectFirst("form[method=post][action$=/request-recovery]");
         assertNotNull(form);
         assertThat(form.attr("action")).isEqualTo(String.format(REQUEST_NET_RECOVERY_ENDPOINT, Long.valueOf(id)));
 
-        assertThat(Objects.requireNonNull(form).selectFirst("button")).isNotNull();
+        Element button = form.selectFirst("button[type=submit]");
+        assertNotNull(button);
     }
 }
